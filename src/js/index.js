1826890215a7da709125b2825d5d7486a393f370
Vladimir <Krizhanovssskiy@gmail.com>
2020-03-18 11:28:10 +0200

    no_tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,43 @@
+(function () {
+  const openBtnPopUpError = [...document.querySelectorAll('.Btn__comeIn')];
+  const closeBtnPopUpError = document.querySelector('.Btn__close');
 
-const btn_prev = document.querySelector('.Gallery .bg-prev');
-const btn_next = document.querySelector('.Gallery .bg-next');
-let i = 0;
+  const popUp = document.querySelector('.Error');
 
-const item = document.querySelectorAll('.Gallery__item');
-  console.log(item);
-btn_prev.addEventListener('click', function () {
-  console.log('prev')
-  item[i].className = 'Gallery__item';
-  i--;
-  if (i < 0) {
-    i = item.length - 1;
+  if (openBtnPopUpError && closeBtnPopUpError) {
+    openBtnPopUpError.map(item => {
+      item.addEventListener('click', openPopUp)
+    });
+    closeBtnPopUpError.addEventListener('click', closePopUp)
+
+    function openPopUp() {
+      popUp.classList.add('PopUp__show')
+    }
+    function closePopUp() {
+      popUp.classList.remove('PopUp__show')
+    }
   }
-  item[i].className = 'Gallery__item Gallery__show';
-});
+})();
 
-btn_next.addEventListener('click', function () {
-  console.log('next')
 
-  item[i].className = 'Gallery__item';
-  i++;
-  if (i >= item.length) {
-    i = 0;
+(function () {
+  const openBtnPopUpPhoto = document.querySelector('.OpenPhotoPopUp');
+  const closeBtnPopUpPhoto = document.querySelector('.Btn__close');
+  const popUpPhoro = document.querySelector('.Photo');
+
+  if (openBtnPopUpPhoto && closeBtnPopUpPhoto) {
+
+    openBtnPopUpPhoto.addEventListener('click', openPopUp)
+    closeBtnPopUpPhoto.addEventListener('click', closePopUp)
+    function openPopUp() {
+      popUpPhoro.classList.add('PopUp__show')
+    }
+    function closePopUp() {
+      popUpPhoro.classList.remove('PopUp__show')
+    }
   }
-  item[i].className = 'Gallery__item Gallery__show';
-});
+})();
 
 
 
-const openBtnPopUp = [...document.querySelectorAll('.Btn__comeIn')];
-const closeBtnPopUp = document.querySelector('.Btn__close');
 
-const popUp = document.querySelector('.PopUp');
-
-openBtnPopUp.map(item => {
-  item.addEventListener('click', openPopUp)
-});
-closeBtnPopUp.addEventListener('click', closePopUp)
-
-function openPopUp() {
-  popUp.classList.add('PopUp__show')
-}
-function closePopUp() {
-  popUp.classList.remove('PopUp__show')
-}
